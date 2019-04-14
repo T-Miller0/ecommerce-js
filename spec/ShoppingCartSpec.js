a6a41ab25900069ca211f423e26a7f2999783f33
Your Name <thomas.j.c.miller@outlook.com>
2019-04-14 16:58:44 +0100

    addVoucher calculates if voucher is valid and applies it to total

--- spec/ShoppingCartSpec.js
+++ spec/ShoppingCartSpec.js
@@ -28,7 +28,34 @@ describe("ShoppingCart", function() {
     const shirt = { productName: 'black shirt', price: 10}
     shoppingCart.addToCart(shoes);
     shoppingCart.addToCart(shirt);
-    console.log(shoppingCart.cart)
     shoppingCart.cartTotal()
     expect(shoppingCart.total).toEqual(15)
-  })
+  });
+
+  it('Calculates voucher for 15off', () => {
+    const shoes = { productName: 'pumps', price: 100, category: "Men's Footwear"}
+    shoppingCart.addToCart(shoes);
+    shoppingCart.cartTotal()
+    shoppingCart.addVoucher("15off")
+
+    expect(shoppingCart.total).toEqual(85)
+  });
+
+  it('Calculates voucher for 10off', () => {
+    const shoes = { productName: 'pumps', price: 60, category: "Men's Footwear"}
+    shoppingCart.addToCart(shoes);
+    shoppingCart.cartTotal()
+    shoppingCart.addVoucher("10off")
+
+    expect(shoppingCart.total).toEqual(50)
+  });
+
+  it('Calculates voucher for 5off', () => {
+    const shoes = { productName: 'pumps', price: 20, category: "Men's Footwear"}
+    shoppingCart.addToCart(shoes);
+    shoppingCart.cartTotal()
+    shoppingCart.addVoucher("5off")
+
+    expect(shoppingCart.total).toEqual(15)
+  });
+})
--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,6 +1,7 @@
 const ShoppingCart = function() {
   this.cart = []
   this.total = 0
+  this.vouchersUsed = null;
 };
 
 ShoppingCart.prototype.addToCart = function (item) {
@@ -17,4 +18,41 @@ ShoppingCart.prototype.cartTotal = function() {
   })
 
   this.total = prices.reduce((grandTotal, currentValue) => grandTotal + currentValue )
+};
+
+ShoppingCart.prototype.applyVoucher = function (voucher, categoryFound) {
+  switch (voucher) {
+    case '15off':
+      if (this.total > 75 && categoryFound) {
+        this.total -= 15
+        this.vouchersUsed = '15off'
+      }
+      break;
+    case '10off':
+      if (this.total > 50) {
+        this.total -= 10
+        this.vouchersUsed = '10off'
+      }
+      break;
+    case '5off':
+      this.total -= 5
+      this.vouchersUsed = '5off'
+    default:
+    return "Voucher Not Found"
+    }
+};
+
+
+
+ShoppingCart.prototype.addVoucher = function (voucher) {
+  let categories = this.cart.map(item => {return item.category})
+  let categoryFound = function() {
+    if (categories.includes("Women's Footwear") || categories.includes("Men's Footwear")) {
+      return true;
+    }
+    return false;
+  }
+  if (this.vouchersUsed === null) {
+    this.applyVoucher(voucher, categoryFound)
+  }
 };
